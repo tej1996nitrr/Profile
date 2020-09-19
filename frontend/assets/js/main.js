dc1c26c761f439d26216775fa3772e8f48e242b4
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-19 19:03:20 +0530

    social icons

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -33,11 +33,20 @@ const sr = ScrollReveal({
     reset: true
 });
 
+
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
 sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+
+// sr.reveal('.home__img',{delay: 400}); 
+// sr.reveal('.home__social-icon',{ interval: 200});
+// sr.reveal('.home__social-icon-instagram',{ interval: 200});  
+// sr.reveal('.home__social-icon-facebook',{ interval: 200});  
+// sr.reveal('.home__social-icon-github',{ interval: 200});  
+// sr.reveal('.home__social-icon-gmail',{ interval: 200});  
+// sr.reveal('.home__social-icon-linkedin',{ interval: 200}); 
+ 
+
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
