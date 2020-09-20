76bea98238a6a178733ede72ddd4ff2552ff3019
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-20 12:15:30 +0530

    models

--- portfolio/core/models.py
+++ portfolio/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Skills(models.Model):
+    skill = models.CharField(max_length=50, unique=True)
+    url = models.URLField()
+
+class Post(models.Model):
+
+    headline = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
+    body = models.TextField(null=True, blank=True)
+	
+    def __str__(self):
+        return self.headline
