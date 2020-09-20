f560c4a54c7f68170d03ceef2f403e04338ffd87
tej1996nitrr <tejaskanikdaley1996@gmail.com>
2020-09-20 12:39:21 +0530

    settings

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/img/'
+
+STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
